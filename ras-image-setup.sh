199d850ca9c3ad7ace027db2872967ef1228d5bd
hveficent <hector.villarreal@eficent.com>
2018-09-20 15:47:24 +0200

    add raspberry pi required services

--- ras-image-setup.sh
+++ ras-image-setup.sh
@@ -8,7 +8,7 @@ apt update && apt install -yq \
     python3-smbus i2c-tools git python3-dev python3-pip \
     libfreetype6-dev libjpeg-dev build-essential libsdl-dev \
     libportmidi-dev libsdl-ttf2.0-dev libsdl-mixer1.2-dev libsdl-image1.2-dev libopenjp2-7 \
-    python3-RPi.GPIO
+    python3-RPi.GPIO dnsmasq hostapd isc-dhcp-server
 apt clean
 rm -rf /var/lib/apt/lists/*
 
@@ -22,9 +22,11 @@ else
 fi
 
 pip3 install -r /home/pi/ras/requeriments.txt
-cp -f /home/pi/ras/config.txt /boot/
-mv /home/pi/ras/credentials.json.sample /home/pi/ras/credentials.json
-mv /home/pi/ras/data.json.sample /home/pi/ras/data.json
+cp -f /home/pi/ras-image-setup/config.txt /boot/
+cp -f /home/pi/ras-image-setup/modules /etc/
+cp -f /home/pi/ras-image-setup/rc.local /etc/
+mv /home/pi/ras/dicts/credentials.json.sample /home/pi/ras/dicts/credentials.json
+mv /home/pi/ras/dicts/data.json.sample /home/pi/ras/dicts/data.json
 
 git clone https://github.com/lthiery/SPI-Py.git
 cd SPI-Py
@@ -32,23 +34,28 @@ python3 setup.py install
 cd ..
 rm -R SPI-Py
 
-bash < curl -L https://github.com/hveficent/resin-wifi-connect/raw/master/scripts/raspbian-install.sh
+# systemctl stop dhcpcd
+# systemctl disable dhcpcd
+
+# systemctl enable NetworkManager
+# systemctl start NetworkManager
 
 # Setup ras portal service
-cp -f /home/pi/ras/ras-portal.service /lib/systemd/system/
+cp -f /home/pi/ras-image-setup/ras-portal.service /lib/systemd/system/
 chmod 755 /lib/systemd/system/ras-portal.service
 chown root: /lib/systemd/system/ras-portal.service
 
+# systemctl enable ras-portal.service
+# systemctl start ras-portal.service
+
 # Setup ras launcher service
-cp -f /home/pi/ras/ras-launcher.service /lib/systemd/system/
+cp -f /home/pi/ras-image-setup/ras-launcher.service /lib/systemd/system/
 chmod 755 /lib/systemd/system/ras-launcher.service
 chown root: /lib/systemd/system/ras-launcher.service
 
-systemctl stop dhcpcd
-systemctl disable dhcpcd
+# systemctl enable ras-launcher.service
+# systemctl start ras-launcher.service
 
-systemctl enable NetworkManager
-systemctl start NetworkManager
+# Setup resin-wifi-connect
 
-systemctl enable ras-portal.service
-systemctl start ras-portal.service
+bash /home/pi/ras-image-setup/wifi-connect-raspbian-install.sh
