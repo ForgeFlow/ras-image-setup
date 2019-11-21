0f9bb482cdd529ae858c55dfad576b5f39f85061
hveficent <hector.villarreal@eficent.com>
2019-11-21 16:27:36 +0100

    Prepare ras3-eficent

--- ras-image-setup.sh
+++ ras-image-setup.sh
@@ -1,47 +1,42 @@
 
 #!/bin/bash
 
-cd /home/pi/
-
 # Install requirements
 apt update && apt install -yq \
     python3-smbus i2c-tools git python3-dev python3-pip \
     libfreetype6-dev libjpeg-dev build-essential libsdl-dev \
     libportmidi-dev libsdl-ttf2.0-dev libsdl-mixer1.2-dev libsdl-image1.2-dev libopenjp2-7 \
-    python3-RPi.GPIO watchdog
-
-if [ ! -d /home/pi/ras ]
-then
-    git clone https://github.com/Eficent/ras.git
-else
-    cd ras
-    git fetch origin
-    git reset --hard origin/master
-fi
-
-pip3 install -r /home/pi/ras/requirements.txt
-cp -f /home/pi/ras-image-setup/config.txt /boot/
-cp -f /home/pi/ras-image-setup/modules /etc/
-cp -f /home/pi/ras-image-setup/rc.local /etc/
-cp -f /home/pi/ras-image-setup/bcm2835-wdt.conf /etc/modprobe.d/bcm2835-wdt.conf
-cp -f /home/pi/ras-image-setup/system.conf /etc/systemd/system.conf
-cp -f /home/pi/ras-image-setup/watchdog.conf /etc/watchdog.conf
-mv /home/pi/ras/dicts/credentials.json.sample /home/pi/ras/dicts/credentials.json
-
-# Setup ras launcher service
-cp -f /home/pi/ras-image-setup/ras-launcher.service /lib/systemd/system/
-chmod 755 /lib/systemd/system/ras-launcher.service
-chown root: /lib/systemd/system/ras-launcher.service
-
-# Setup resin-wifi-connect
-apt update && \
-apt install -y -d network-manager && \
-apt install -y network-manager 
-sh /home/pi/ras-image-setup/wifi-connect-raspbian-install.sh
-
-cp -rf /home/pi/ras-image-setup/ui/img  /usr/local/share/wifi-connect/ui/
-cp -f /home/pi/ras-image-setup/ui/index.html /usr/local/share/wifi-connect/ui/index.html
-
+    python3-RPi.GPIO watchdog hostapd dnsmasq
 apt -yq clean
 apt -yq autoremove
 rm -rf /var/lib/apt/lists/*
+
+# Clone oot project
+cd /home/pi/
+git clone https://github.com/tegin/oot
+cd oot
+git checkout dev-xmlrpc
+pip3 install -r requirements.txt
+pip3 install .
+
+
+# Clone ras project
+git clone https://github.com/eficent/ras.git
+cd ras
+git checkout ras3-eficent
+pip3 install cmdline PyGithub luma.oled
+
+
+# Configure settings
+cp -f /home/pi/ras-image-setup/config.txt /boot/
+cp -f /home/pi/ras-image-setup/modules /etc/
+cp -f /home/pi/ras-image-setup/bcm2835-wdt.conf /etc/modprobe.d/bcm2835-wdt.conf
+cp -f /home/pi/ras-image-setup/system.conf /etc/systemd/system.conf
+cp -f /home/pi/ras-image-setup/watchdog.conf /etc/watchdog.conf
+
+# Setup ras service
+cp -f /home/pi/ras/ras.service /lib/systemd/system/
+systemctl enable ras.service
+
+# Disable hostapd
+systemctl disable hostapd
